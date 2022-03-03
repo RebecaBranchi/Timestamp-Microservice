8000581db508cb2ad421fb84b701b9d8b4ee44fb
RebecaBranchi <rebecabranchi@gmail.com>
2022-03-02 21:17:19 -0300

    se agregan items de fechas

---
+++ server.js
@@ -0,0 +1,67 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api", (req, res)=> {
+  res.json({
+    "unix" : Date.now(),
+    "utc" : Date()
+  })
+})
+
+
+app.get("/api/timestamp", (req, res)=> {
+  var now = new Date();
+  res.json({
+    "unix": now.getTime(),
+    "utc": now.toISOString()
+  });
+});
+
+app.get("/api/:date_string", (req, res)=> {
+let dateString =  req.params.date_string;
+
+if (parseInt(dateString)> 10000) {
+  let unixTime = new Date(parseInt(dateString));
+   res.json ({
+    "unix": unixTime.getTime(),
+    "utc" : unixTime.toUTCString()
+  })
+}
+  
+let passedInValue = new Date(dateString);
+
+if (passedInValue == "Invalid Date") {
+res.json ({ "error" : "Invalid Date" });  
+} else {
+  res.json ({
+    "unix": passedInValue.getTime(),
+    "utc" : passedInValue.toUTCString()
+  })
+}
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
